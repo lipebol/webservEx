687fe825d20fdee2db8d08d9b7c73d9473463317
lipebol <lipe_bol5@hotmail.com>
2024-09-13 02:47:24 +0000

    add 'cors'

--- webservEx_GraphQL/server.js
+++ webservEx_GraphQL/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const Express = require("express")
+const cors = require("cors")
 
 
 const Server = Express()
 
+Server.use(cors())
 
 Server.get("/", (req, res) => {
     res.status(200).json({message: "WooooHooooo!"})
