810cfa743d1759f03943791990ea0250881a3d8d
lipebol <lipe_bol5@hotmail.com>
2024-09-16 18:52:50 -0300

    rm method 'getAll' and changed name 'repo'

--- webservEx_REST/src/main/java/dev/lipebol/webservEx_REST/services/Service_Tracks.java
+++ webservEx_REST/src/main/java/dev/lipebol/webservEx_REST/services/Service_Tracks.java
@@ -1,9 +1,6 @@
 package dev.lipebol.webservEx_REST.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import dev.lipebol.webservEx_REST.models.Model_Tracks;
@@ -13,20 +10,13 @@ import dev.lipebol.webservEx_REST.repositories.Repo_Tracks;
 public class Service_Tracks { 
 	
 	@Autowired
-	private Repo_Tracks tracks;
+	private Repo_Tracks r_tracks;
 
-	public List<Model_Tracks> getAll() {
-		Sort sorted = Sort.by("artist").ascending().and(
-				Sort.by("title").ascending()
-			 );
-		return tracks.findAll(sorted);
-	}
-	
 	public Model_Tracks create(Model_Tracks track) {
-		return tracks.save(track);
+		return r_tracks.save(track);
 	}
 	
 	public Model_Tracks update(Model_Tracks track) {
-		return tracks.save(track);
+		return r_tracks.save(track);
 	}
 }
