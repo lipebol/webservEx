d953c7ab34912212545aac6b7b3f1fc2486a99ba
lipebol <lipe_bol5@hotmail.com>
2024-09-23 03:33:21 +0000

    add 'helmet', routes 'graphql' and '404'

--- webservEx_GraphQL/server.js
+++ webservEx_GraphQL/server.js
@@ -1,14 +1,29 @@
 require('dotenv').config()
-const Express = require("express")
-const cors = require("cors")
-
+const cors = require(process.env.CORS)
+const Express = require(process.env.SERVER)
+const helmet = require(process.env.HELMET)
+const graphql = require(process.env.GRAPHQL_MAIN)
 
 const Server = Express()
 
 Server.use(cors())
+Server.use(
+    helmet(
+        {
+            contentSecurityPolicy: (
+                process.env.NODE_ENV === process.env.MODE
+            ) ? undefined : false
+        }
+    )
+)
 
-Server.get("/", (req, res) => {
-    res.status(200).json({message: "WooooHooooo!"})
-})
+
+Server.use(process.env.ROUTE_GRAPHQL, graphql)
+
+Server.use(
+    (req, res) => {
+        return res.status(404).json({ error: process.env.NOT_FOUND })
+    }
+)
 
 Server.listen(process.env.SERVER_PORT)
