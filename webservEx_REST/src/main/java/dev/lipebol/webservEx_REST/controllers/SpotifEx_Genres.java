44f1190a85b7645a968d188a984eb7322f54594d
lipebol <lipe_bol5@hotmail.com>
2024-10-01 21:02:30 -0300

    add 'status code' and 'delete route'

--- webservEx_REST/src/main/java/dev/lipebol/webservEx_REST/controllers/SpotifEx_Genres.java
+++ webservEx_REST/src/main/java/dev/lipebol/webservEx_REST/controllers/SpotifEx_Genres.java
@@ -1,13 +1,18 @@
 package dev.lipebol.webservEx_REST.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.lipebol.webservEx_REST.models.Model_Genres;
@@ -23,18 +28,29 @@ public class SpotifEx_Genres {
 	private Service_Genres s_genres;
 	
 	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
 	public List<Model_Genres>  allGenres(){
 		return s_genres.getAll();
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Model_Genres create(@RequestBody Model_Genres genre) {
 		return s_genres.create(genre);
 	}
 	
 	@PutMapping
+	@ResponseStatus(HttpStatus.OK)
 	public Model_Genres update(@RequestBody Model_Genres genre) {
 		return s_genres.update(genre);
 	}
-
+	
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public Map<String, String> notDelete() {
+		Map<String, String> map = new HashMap<>();
+		map.put("error", "Sorry! Not Found.");
+		return map;
+	}
+	
 }
